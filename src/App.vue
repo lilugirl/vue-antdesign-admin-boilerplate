d3db69b14521bcd908ef08c5d8ef548b3d005057
刘毅 <liuyi1@chint.com>
2020-04-02 16:42:45 +0800

    增加业务国际化

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
   <div id="app">
+   
+      <div class="locale-changer">
+         切换语言 
+    <select v-model="$i18n.locale">
+      <option v-for="(lang, i) in langs" :key="`Lang${i}`" :value="lang">{{ lang }}</option>
+    </select>
+  </div>
+   {{ $t("message.hello") }}
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -7,6 +15,16 @@
     <router-view />
   </div>
 </template>
+
+<script>
+
+export default {
+  data () {
+    return { langs: ['zh','ja', 'en'] }
+  }
+}
+ 
+</script>
 
 <style lang="stylus">
 #app
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from "vue";
+import VueI18n from "vue-i18n";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
@@ -7,9 +8,38 @@ import "ant-design-vue/dist/antd.css";
 
 Vue.config.productionTip = false;
 Vue.use(Antd);
+Vue.use(VueI18n);
 
-new Vue({
+// 准备翻译的语言环境信息
+const messages = {
+  zh: {
+    message: {
+      hello: "你好 世界"
+    }
+  },
+  en: {
+    message: {
+      hello: "hello world"
+    }
+  },
+  ja: {
+    message: {
+      hello: "こんにちは、世界"
+    }
+  }
+};
+
+// 通过选项创建 VueI18n 实例
+const i18n = new VueI18n({
+  locale: "ja", // 设置地区
+  messages // 设置地区信息
+});
+
+const app = new Vue({
+  i18n,
   router,
   store,
   render: h => h(App)
 }).$mount("#app");
+
+Vue.prototype.$i18n = app.$i18n;
